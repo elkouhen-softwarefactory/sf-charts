089389bed116b09ea9324cc658f4f574c3ea9259
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-11-16 11:00:40 +0100

    commit

--- release.sh
+++ release.sh
@@ -2,20 +2,7 @@
 
 version=0.1.58
 
-#helm package --version $version api-gateway
-#helm package --version $version evenement-parcours-integration
-#helm package --version $version evenement-rappel
-#helm package --version $version gestion-evenement
-#helm package --version $version gestion-images
-#helm package --version $version gestion-personnes
-#helm package --version $version parcours-integration
-#helm package --version $version referentiel-personnes-api
-#helm package --version $version referentiel-personnes-mock
-#helm package --version $version referentiel-personnes-ui
-#helm package --version $version grav
-#helm package --version $version skills-api
-
-helm package --version $version elasticsearch
+helm package --version $version ELK
 
 helm repo index . --url https://softeamouest.github.io/charts/
 
