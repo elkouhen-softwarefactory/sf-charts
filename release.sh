2764f885245c6dd6dd29c288293572c88f01b3db
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-11-28 21:34:45 +0100

    commit

--- release.sh
+++ release.sh
@@ -3,6 +3,8 @@
 version=0.1.58
 
 helm package --version $version ELK
+helm package --version $version h2
+helm package --version $version helloworld
 
 helm repo index . --url https://softeamouest.github.io/charts/
 
