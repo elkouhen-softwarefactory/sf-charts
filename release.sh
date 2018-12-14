6591e2978a70bcbb007b8cf0d4e4b2868ed722b2
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-12-14 11:24:05 +0100

    wip

--- release.sh
+++ release.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-while getopts "v:" arg; do
+while getopts "v:p:" arg; do
   case $arg in
     v)
       version=$OPTARG
       ;;
+    p)
+      package=$OPTARG
+      ;;
   esac
 done
 
-helm package --version $version elk
-helm package --version $version h2
-helm package --version $version books-api
+helm package --version $version $package
+#helm package --version $version h2
+#helm package --version $version books-api
 
 helm repo index . --url https://softeamouest-opus.github.io/charts/
 
