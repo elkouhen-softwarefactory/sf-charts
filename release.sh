ccc5a016c9d8724010a1df5c79bb44c69a434335
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-12-28 10:42:53 +0100

    release chart elasticstack:0.0.4

--- release.sh
+++ release.sh
@@ -11,14 +11,18 @@ while getopts "v:c:" arg; do
   esac
 done
 
+cd $package
+
+helm dependency update --skip-refresh
+
+cd -
+
 helm package --version $version $package
-#helm package --version $version h2
-#helm package --version $version books-api
 
 helm repo index . --url https://softeamouest-opus.github.io/charts/
 
 git add .
 
-git commit -am commit
+git commit -am "release chart $package:$version"
 
 git push
