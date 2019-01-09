be8e6de79bfd0e42cf2c3dbda7fa2bc5ffcf9707
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2019-01-09 17:11:41 +0100

    wip

--- commit.sh
+++ commit.sh
@@ -21,12 +21,6 @@ echo "machine github.com" > ~/.netrc
 echo "        login $username" >> ~/.netrc
 echo "        password $password" >> ~/.netrc
 
-echo "StrictHostKeyChecking no" > ~/.ssh/config
-
-#chmod 600 ~/.ssh/id_rsa
-#chmod 644 ~/.ssh/id_rsa.pub
-#chmod 644 ~/.ssh/config
-
 git add .
 
 git config --global user.email "startech.ouest@gmail.com"
